f97a864a175368d2588dcc6b9239f870fd5e7c54
noahwillgohs <willgohsnoah@gmail.com>
2025-04-29 22:11:14 -0500

    Assignment 4

---
+++ app/src/main/java/com/example/skyward/ForecastView.kt
@@ -0,0 +1,132 @@
+package com.example.skyward
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.skyward.models.ForecastData
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import kotlin.math.roundToInt
+
+@Composable
+fun ForecastScreen(viewModel: WeatherViewModel, onBackClick: () -> Unit) {
+
+    val forecastData = viewModel.forecastList.observeAsState()
+    val fCityName = viewModel.fCityName.observeAsState()
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .safeContentPadding()
+            .background(
+                Brush.verticalGradient(
+                colors = listOf(colorResource(R.color.purple_200), colorResource(R.color.teal_200))),
+                RoundedCornerShape(12.dp)
+            )
+    ){
+        Button(
+        onClick = { onBackClick() },
+        modifier = Modifier
+            .padding(bottom = 12.dp),
+    ) {
+        Text(stringResource(R.string.back))
+    }
+        forecastData.value?.let { forecast ->
+            Column ( modifier = Modifier
+                .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = fCityName.value.toString(),
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(forecast) { forecastData ->
+                        ForecastItem(forecastData)
+                    }
+                }
+            }
+        } ?: Text(text = stringResource(R.string.loading))
+
+    }
+}
+
+@Composable
+fun ForecastItem(forecastData: ForecastData.Forecast) {
+    val date = SimpleDateFormat("EEE, MMM d",
+        Locale.getDefault()).format(Date(forecastData.date * 1000))
+    val icon = forecastData.fWeather[0].icon
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(1.dp, Color.Black, shape = RoundedCornerShape(12.dp))
+
+            .safeContentPadding(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.weight(1f)
+                .padding(8.dp)) {
+            Text(
+                text = date,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = stringResource(R.string.high) + " " + forecastData.temp.max.roundToInt() + "°",
+                fontSize = 18.sp
+            )
+            Text(
+                text = stringResource(R.string.low) + " " + forecastData.temp.min.roundToInt() + "°",
+                fontSize = 18.sp
+            )
+        }
+
+        Image(
+            painter = painterResource(getWeatherIcon(icon)),
+            contentDescription = null,
+            modifier = Modifier
+                .size(82.dp)
+                .padding(12.dp),
+            contentScale = ContentScale.Fit
+        )
+    }
+}
--- app/src/main/java/com/example/skyward/MainActivity.kt
+++ app/src/main/java/com/example/skyward/MainActivity.kt
@@ -4,9 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.skyward.repositories.WeatherRepository
 import com.example.skyward.services.RetrofitInstance
 import com.example.skyward.ui.theme.SkywardTheme
@@ -22,13 +31,37 @@ class MainActivity : ComponentActivity() {
             weatherRepository,
             apiKey = resources.getString(R.string.weather_api_key)
         )
-        weatherViewModel.fetchCurrentWeather("44.95", "-93.09")
+
+        weatherViewModel.fetchCurrentWeather("55024")
         enableEdgeToEdge()
         setContent {
-            SkywardTheme {
-                Scaffold (modifier = Modifier.fillMaxSize()) { innerPadding ->
-                        MainScreen(viewModel = weatherViewModel)
+            var showForecast by remember { mutableStateOf(false) }
+            var zipCode by remember { mutableStateOf("55024") }
 
+            SkywardTheme {
+                if(showForecast) {
+                    Scaffold(modifier = Modifier.fillMaxSize()
+                        .background(colorResource(R.color.purple_200))) { innerPadding ->
+                        ForecastScreen(
+                            viewModel = weatherViewModel,
+                            onBackClick = { showForecast = false })
+                    }
+                }
+                else
+                    Scaffold (modifier = Modifier.fillMaxSize()
+                        .background(colorResource(R.color.purple_500))) { innerPadding ->
+                        MainScreen(
+                            viewModel = weatherViewModel,
+                            onForecastClick = {
+                                weatherViewModel.fetchForecastWeather(zipCode)
+                                showForecast = true
+                            },
+                            onZipSearch = { newZipCode ->
+                                zipCode = newZipCode
+                                weatherViewModel.fetchCurrentWeather(zipCode)
+                                weatherViewModel.fetchForecastWeather(zipCode)
+                            }
+                        )
                 }
             }
         }
